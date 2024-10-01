f7706b4a40be6c03fe212c8a20fd792580fb860d
Leonardo <lfm.fiap@gmail.com>
2024-09-30 21:43:44 -0300

    Resolução do exercício 2.

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ let valor2 = null
 console.log(valor2)
 
 // Com Valor
-let valor3 = CheckPoint1
+let valor3 = "CheckPoint1"
 console.log(valor3)
+
+//2)
+
+let varString = "6541.968"
+console.log(varString)
+console.log(parseFloat(varString));
+
+//3)
+
+
+//4)
+//5)
+//6)
